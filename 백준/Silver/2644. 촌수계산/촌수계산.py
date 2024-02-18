5cf26c524997904a0a5c24b35af4c8ca3cee8221
최세연 (Lena) <choi20014830@gmail.com>
2024-02-19 06:55:22 +0900

    [Silver II] Title: 촌수계산, Time: 64 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,31 +1,34 @@
 from collections import deque
 
-def bfs(s):
-    queue=deque([s])
-    visited[s]=True
+n = int(input())
+A, B = map(int,input().split())
+m = int(input())
 
-    while queue:
-        v=queue.popleft()
-
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                res[i]=res[v]+1
-                visited[i]=True
-
-n=int(input())
-A,B=map(int,input().split())
-m=int(input())
-
-graph=[[] for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
 visited = [False] * (n + 1)
-res=[0]*(n+1)
+res = [0] * (n+1)
 
 
 for _ in range(m):
-    a,b=map(int,input().split())
+    a, b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
+
+def bfs(v):
+    visited[v] = True
+    q = deque()
+    q.append(v)
+    
+    while q:
+        n = q.popleft()
+        if n == B:
+            return
+        
+        for i in graph[n]:
+            if not visited[i]:
+                visited[i] = True
+                res[i] = res[n] + 1
+                q.append(i)
 
 bfs(A)
 if res[B]>0:
