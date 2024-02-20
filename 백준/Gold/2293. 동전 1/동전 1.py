5ffb3d69d3f049864e42a29cc59e04084df49bc1
최세연 (Lena) <choi20014830@gmail.com>
2024-02-20 20:39:12 +0900

    [Gold V] Title: 동전 1, Time: 180 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+
+coins = []
+
+for _ in range(N):
+    coins.append(int(input()))
+
+dp = [0] * (K + 1)
+dp[0] = 1
+
+for coin in coins:
+    for i in range(coin, K + 1):
+        dp[i] += dp[i - coin]
+
+print(dp[-1])
